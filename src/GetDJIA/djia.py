73a9b9a3fdb97d9e4afadae4e06e83368ad98e24
Yukai Tu <tuyukai1994@gmail.com>
2017-11-03 10:00:33 -0700

    Add GetTweets module

--- src/GetDJIA/djia.py
+++ src/GetDJIA/djia.py
@@ -3,3 +3,7 @@ from datetime import datetime
 
 google = pdr.get_data_yahoo(symbols='GOOG', start=datetime(2017, 1, 1), end=datetime(2017, 10, 1))
 print(google)
+
+
+g = pdr.get_data_yahoo(symbols='GOOG')
+print(g)
