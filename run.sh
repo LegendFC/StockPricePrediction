47104a74c92013e479cb1213cf21200fcc99f199
Yukai Tu <tuyukai1994@gmail.com>
2017-12-07 22:45:44 -0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-python src/GetTweets/getOldTweets.py --querysearch="Google" --since 2017-11-01 --until 2017-11-02 --stdout=True --isJson=True --output=sample_tweets.json --maxtweets=100
+python src/GetTweets/getOldTweets.py --querysearch="Texas Instruments" --since="2016-12-01" --until="2017-12-01" --output="TXN_data.txt"
+python src/GetDJIA/djia.py --symbol='TXN' --since='2016-12-01' --until='2017-12-01' --output="TXN_price.txt"
+python src/Analysis/lr_svm_model.py
+python src/Analysis/NN_Model_for_emotion.py
+python src/Analysis/NN_Model_for_price.py
